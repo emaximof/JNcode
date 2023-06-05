226e82b0692943a97670c03257f81056c914f559
emaximof <emaximof@salesforce.com>
2023-06-05 16:19:22 +0300

    comment

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-import openai
+#import openai
 
 print(4)
 print('test')
 
 
-# Set up your API key
-openai.api_key = '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'
-
-# Send a question to ChatGPT
-question = 'What is the capital of France?'
-response = openai.Completion.create(
-    engine='text-davinci-003',  # Specify the model to use
-    prompt=question,
-    max_tokens=50,  # Control the response length
-    n=1,  # Generate a single response
-    stop=None,  # Can specify a stopping condition for the model
-)
-
-# Print the answer
-answer = response.choices[0].text.strip()
-print(answer)
+# # Set up your API key
+# openai.api_key = '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'
+#
+# # Send a question to ChatGPT
+# question = 'What is the capital of France?'
+# response = openai.Completion.create(
+#     engine='text-davinci-003',  # Specify the model to use
+#     prompt=question,
+#     max_tokens=50,  # Control the response length
+#     n=1,  # Generate a single response
+#     stop=None,  # Can specify a stopping condition for the model
+# )
+#
+# # Print the answer
+# answer = response.choices[0].text.strip()
+# print(answer)
 
 
 
