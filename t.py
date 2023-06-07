709cc3f616f2c944dd64591f244a5d1705b48776
Shay Gozansky <sgozansky@sgozans-ltmkwun.internal.salesforce.com>
2023-06-07 10:03:04 +0300

    add some script

---
+++ t.py
@@ -0,0 +1,39 @@
+x = open('/Users/sgozansky/Downloads/cucumberWithFailed.json', 'r')
+ 
+def remove_fields(json_obj, field_name):
+    if isinstance(json_obj, dict):
+        # Check if the field_name exists in the dictionary
+        if field_name in json_obj:
+            del json_obj[field_name]
+        
+        # Recursively call the function on each value in the dictionary
+        for key, value in list(json_obj.items()):
+            remove_fields(value, field_name)
+            
+    elif isinstance(json_obj, list):
+        # Recursively call the function on each element in the list
+        
+        for item in json_obj:
+            remove_fields(item, field_name)
+        
+        
+def remove_elemts(obj):
+    new_elements = []
+    elemnts = obj['elements']
+    for element in elemnts:
+        if element['type'] == "scenario":
+            new_elements.append(element)
+    obj['elements'] = new_elements
+            
+import json
+json_obj = json.load(x)
+remove_fields(json_obj, 'data')
+remove_fields(json_obj, 'steps')
+remove_fields(json_obj, 'duration')
+remove_fields(json_obj, 'match')
+remove_fields(json_obj, 'output')
+remove_fields(json_obj, 'after')
+remove_fields(json_obj, 'error_message')
+remove_fields(json_obj, 'embeddings')
+remove_elemts(json_obj)
+print(json.dumps(json_obj))
